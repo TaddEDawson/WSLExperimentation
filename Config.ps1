a2111a3d36e1698010a0b43c896cb24eb699925b
Tadd E. Dawson <tadd@microsoft.com>
2020-04-02 14:27:29 -0400

    document steps taken

--- Config.ps1
+++ Config.ps1
@@ -1,3 +1,25 @@
+<#
+Followed the steps from https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Update the list of products
+sudo apt-get update
+
+# Enable the "universe" repositories
+sudo add-apt-repository universe
+
+# Install PowerShell
+sudo apt-get install -y powershell
+
+# Start PowerShell
+pwsh
+
+#>
+Set-Location /home/tadd/dev/
 git config --global user.email "tadd@microsoft.com"
 git config --global user.name "Tadd E. Dawson"
 git remote add origin https://github.com/TaddEDawson/WSLExperimentation.git
