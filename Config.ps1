cd9179cc3684df7a7a8c7b4ee7fd786654653031
Tadd E. Dawson <tadd@microsoft.com>
2020-04-02 14:16:26 -0400

    add push

--- Config.ps1
+++ Config.ps1
@@ -1,3 +1,4 @@
 git config --global user.email "tadd@microsoft.com"
 git config --global user.name "Tadd E. Dawson"
 git remote add origin https://github.com/TaddEDawson/WSLExperimentation.git
+git push -u origin master
