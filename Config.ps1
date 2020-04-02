c3a0c97f28873a3e8528c75b198cd679cbc558ea
Tadd E. Dawson <tadd@microsoft.com>
2020-04-02 14:18:20 -0400

    notes after successful push

--- Config.ps1
+++ Config.ps1
@@ -2,3 +2,16 @@ git config --global user.email "tadd@microsoft.com"
 git config --global user.name "Tadd E. Dawson"
 git remote add origin https://github.com/TaddEDawson/WSLExperimentation.git
 git push -u origin master
+<#
+Username for 'https://github.com': TaddEDawson
+Password for 'https://TaddEDawson@github.com':
+Counting objects: 10, done.
+Delta compression using up to 8 threads.
+Compressing objects: 100% (9/9), done.
+Writing objects: 100% (10/10), 940 bytes | 235.00 KiB/s, done.
+Total 10 (delta 2), reused 0 (delta 0)
+remote: Resolving deltas: 100% (2/2), done.
+To https://github.com/TaddEDawson/WSLExperimentation.git
+ * [new branch]      master -> master
+Branch 'master' set up to track remote branch 'master' from 'origin'.
+#>
